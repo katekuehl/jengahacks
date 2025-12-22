26d7f046c25660d5009dcf56c76ab4d4a46ad8e3
katekuehl <katekuehl@gmail.com>
2025-12-21 18:06:11 -0600

    Remove BlogPreview component from Index page to streamline layout and improve loading performance.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Sponsors from "@/components/Sponsors";
-import BlogPreview from "@/components/BlogPreview";
 import Registration from "@/components/Registration";
 import Footer from "@/components/Footer";
 import SEO from "@/components/SEO";
@@ -27,9 +26,6 @@ const Index = () => {
             <Sponsors />
           </ScrollReveal>
           <ScrollReveal direction="up" delay={300}>
-            <BlogPreview />
-          </ScrollReveal>
-          <ScrollReveal direction="up" delay={400}>
             <Registration />
           </ScrollReveal>
         </main>
